da493d411b32fb96eb3556bb443fbd800c8c19d8
Oliver Bagley <7492412+OliverBagley@users.noreply.github.com>
2025-08-26 16:03:57 +0100

    Setup vitepress docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+
+export default {
+  base: '/docs/', // 👈 important for GitHub Pages subpath
+  title: "My Docs",
+  description: "VitePress site on GitHub Pages",
+}
